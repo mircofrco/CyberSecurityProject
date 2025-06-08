4e0e7a80e7806be116aba09551911c2149e15fae
Niklas Hasenkopf <n.hasenkopf@stud.unibas.ch>
2025-06-08 14:59:58 +0200

    fixed api

--- app/api/main.py
+++ app/api/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from app.api.auth.router import router as auth_router
 from app.api.auth.mfa_router import router as mfa_router
 from app.api.admin.router import router as admin_router
 
 app = FastAPI(title="SecureVote")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # Or ["*"] to allow all origins (dev only)
+    allow_credentials=True,
+    allow_methods=["*"],  # Or ["POST", "GET", "OPTIONS", etc.]
+    allow_headers=["*"],
+)
 
 app.include_router(auth_router)
 app.include_router(mfa_router)
